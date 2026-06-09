6974a40cb10c50cc7f4c374d413b4a9b734d4e01
lelegabriele006-dot <lele.gabriele006@gmail.com>
2026-06-09 17:40:41 +0200

    Change bot activity type from Playing to Streaming

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | /Nexus Bot").
         name: "type /help for command list, made whith ❤️",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
